9765be5e3e70524e1169c39d54037d34aeac6017
HugoPC <hugothiebaut22@gmail.com>
2023-11-27 15:11:00 +0100

    Player Movement + Camera Controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sr;
+    private float dirX = 0f;
+    private float dirY = 0f;
+
+    private enum MovementState { idle, running};
+
+    [SerializeField] private float moveSpeed = 7f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        dirY = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(dirX, dirY).normalized; // Normalize to ensure consistent movement speed in all directions
+
+        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+
+        MovementState state;
+
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sr.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sr.flipX = true;
+        }
+        else if (dirY > 0f) {
+            state = MovementState.running;
+        }
+        else if (dirY < 0f) {
+            state = MovementState.running;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        animator.SetInteger("state", (int)state);
+    }
+
+}
