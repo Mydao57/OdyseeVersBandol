cdad19b945d19895119b2803ca19c0eaef75313e
DELACROIX Luc <luc.delacroix3@etu.univ-lorraine.fr>
2024-01-09 21:27:54 +0100

    ajout TriggerZoneBoss zone boss

---
+++ Assets/Scripts/Map/TriggerZoneBoss.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TriggerZoneBoss : MonoBehaviour
+{
+    public GameObject bossPrefab;
+    public List<Transform> spawnPoints = new List<Transform>();
+    public GameObject WallLeft;
+
+    private bool triggerActivated = false;
+    private bool gameActive = false;
+
+    void Start()
+    {
+        SetWallsActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!triggerActivated && other.CompareTag("Player"))
+        {
+            SetWallsActive(true);
+            ActivateMiniBoss(); // Appel de la fonction pour faire apparaître le boss
+            triggerActivated = true;
+            gameActive = true;
+        }
+    }
+
+    void ActivateMiniBoss()
+    {
+            Instantiate(bossPrefab, spawnPoints[0].position, Quaternion.Euler(0f, 180f, 0f));
+    }
+
+
+    void SetWallsActive(bool active)
+    {
+        if (WallLeft != null)
+        {
+            WallLeft.SetActive(active);
+        }
+    }
+
+    void Update()
+    {
+        if (gameActive && triggerActivated)
+        {
+            CheckForNoEnemies();
+        }
+    }
+
+    void CheckForNoEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (enemies.Length == 0)
+        {
+            // Le code à exécuter lorsque tous les ennemis sont éliminés, si nécessaire.
+        }
+    }
+}
