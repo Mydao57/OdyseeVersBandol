a184911d77904a2b3e1b652888fd724ef1b88a29
DELACROIX Luc <luc.delacroix3@etu.univ-lorraine.fr>
2024-01-11 17:41:59 +0100

    modif obstacle

--- Assets/Scripts/Map/Generate Obstacles.cs
+++ Assets/Scripts/Map/Generate Obstacles.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab; 
+    public GameObject obstaclePrefab;
+    public GameObject obstaclePrefab2;
     public List<Transform> spawnPointsListLeft = new List<Transform>();
     public List<Transform> spawnPointsListRight = new List<Transform>();
     public int probability = 25;
@@ -11,12 +12,12 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         SpawnRandomObstacles(spawnPointsListLeft, true);
-        SpawnRandomObstacles(spawnPointsListRight, false); 
+        SpawnRandomObstacles(spawnPointsListRight, false);
     }
 
     void SpawnRandomObstacles(List<Transform> spawnPoints, bool invertPrefab)
     {
-        int numSpawnPointsToUse = Mathf.CeilToInt(spawnPoints.Count * (probability/100.0f));
+        int numSpawnPointsToUse = Mathf.CeilToInt(spawnPoints.Count * (probability / 100.0f));
 
         List<int> selectedIndices = new List<int>();
 
@@ -34,13 +35,15 @@ public class SpawnManager : MonoBehaviour
         {
             if (selectedIndices.Contains(i))
             {
+                GameObject selectedPrefab = Random.Range(0, 2) == 0 ? obstaclePrefab : obstaclePrefab2;
+
                 if (invertPrefab && spawnPoints == spawnPointsListLeft)
                 {
-                    Instantiate(obstaclePrefab, spawnPoints[i].position, Quaternion.Euler(0, 180, 0));
+                    Instantiate(selectedPrefab, spawnPoints[i].position, Quaternion.Euler(0, 180, 0));
                 }
                 else
                 {
-                    Instantiate(obstaclePrefab, spawnPoints[i].position, Quaternion.identity);
+                    Instantiate(selectedPrefab, spawnPoints[i].position, Quaternion.identity);
                 }
             }
         }
